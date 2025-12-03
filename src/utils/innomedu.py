a2c147d3145ef498dd59866bd83dceb9076c96ca
Demetrius <thedioxider@mail.ru>
2025-12-04 00:14:08 +0300

    docs(innomedu): methods

--- src/utils/innomedu.py
+++ src/utils/innomedu.py
@@ -18,13 +18,20 @@ from utils.conveyor import InnoConveyor
 class InnoMEdu:
     logger = logging.getLogger("InnoMEdu")
     coordinate_tool: str = "tool0"
+    audio_files = {"sound.wav", "start.wav", "finish.wav", "wait.wav"}
 
+    """Joint states of the manipulator from base to end-effector."""
     pose: tuple[float, float, float] = (0.0, 0.0, 0.0)
+    """Cartesian position of end-effector of the manipulator."""
     position: tuple[float, float, float] = (0.0, 0.0, 0.0)
+    """States of GPIO pins by names."""
     gpio_states: dict[str, float] = {}
 
+    # useless but required
     _USELESS_ROTATE = MoveCoordinatesParamsOrientation(0.0, 0.0, 0.0, 1.0)
+    # accuracy of positioning
     _EPS = 0.005
+    # path where audio files stored on manipulator
     _AUDIO_REMOTE_PATH = "/home/promobot"
 
     def __init__(self, host: str, client_id: str, login: str, password: str):
@@ -78,9 +85,15 @@ class InnoMEdu:
         self.logger.info(f"Moved to {self.position}; pose: {self.pose}")
 
     def to_coordinates_task(
-        self, x: float, y: float, z: float, task: Callable[[], None]
+        self, x: float, y: float, z: float, task: Callable[[], None], freq: int = 1
     ):
+        """
+        Stream coordinates to manipulator and simultaneously do something else.
+        task: actions to do while moving to the point; shouldn't be anything heavy
+        freq: how many steps to wait before repeating the action
+        """
         self.medu.set_servo_control_type(ServoControlType.POSE)
+        rep_i = 0
         while (
             abs(self.position[0] - x) > self._EPS
             or abs(self.position[1] - y) > self._EPS
@@ -89,41 +102,54 @@ class InnoMEdu:
             self.medu.stream_coordinates(
                 MoveCoordinatesParamsPosition(x, y, z), self._USELESS_ROTATE
             )
-            task()
+            rep_i = (rep_i + 1) % freq
+            if rep_i == 0:
+                task()
             sleep(0.025)
         self.logger.info(f"Moved to {self.position}; pose: {self.pose}")
 
     def to_coordinates_gripper(
         self, x: float, y: float, z: float, gripper_angle: int, freq: int = 1
     ):
-        grip_i = 0
+        """
+        Go to the point while preserving absolute gripper angle. Be careful,
+        since the angle may be unreachable at some moment on the path.
+        gripper_angle: angle to preserve
+        freq: how frequently to ensure the gripper angle
+        """
 
         def task():
-            nonlocal grip_i
-            grip_i = (grip_i + 1) % freq
-            if grip_i == 0:
-                deg = int(
-                    min(
-                        90.0,
-                        max(
-                            -45.0,
-                            gripper_angle
-                            # + math.atan2(self.position[1], self.position[0]) / math.pi * 180,
-                            + self.pose[0],
-                        ),
-                    )
+            deg = int(
+                min(
+                    90.0,
+                    max(
+                        -45.0,
+                        gripper_angle
+                        # + math.atan2(self.position[1], self.position[0]) / math.pi * 180,
+                        + self.pose[0],
+                    ),
                 )
-                self.medu.manage_gripper(rotation=deg)
+            )
+            self.medu.manage_gripper(rotation=deg)
 
-        self.to_coordinates_task(x, y, z, task)
+        self.to_coordinates_task(x, y, z, task, freq=freq)
 
     def play_audio(self, audio: str):
+        """
+        Play some audio on the manipulator. Need to be loaded on the manipulator,
+        which is checked in the `self.audio_files` list.
+        """
+        if audio not in self.audio_files:
+            self.logger.error(f"No such audio file loaded: {audio}")
         try:
             self.medu.play_audio(audio)
         except Exception as e:
             self.logger.error(f"Playing audio failed with {type(e).__name__}: {e}")
 
     def load_audio(self, source: str):
+        """
+        Load an audio file on the manipulator and add it to the list.
+        """
         if not os.path.isfile(source):
             self.logger.error(f"File {source} does not exist")
             return
@@ -138,6 +164,7 @@ class InnoMEdu:
                     source,
                     os.path.join(self._AUDIO_REMOTE_PATH, os.path.basename(source)),
                 )
+                self.audio_files.add(os.path.basename(source))
                 self.logger.info(f"Successfully loaded {source}")
         except Exception as e:
             self.logger.exception(f"{type(e).__name__}: {e}")
